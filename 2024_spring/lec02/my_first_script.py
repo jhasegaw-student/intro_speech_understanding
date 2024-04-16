1e0a1b361db1b757bba62a529ce28bcd4cbe2c47
jhasegaw-student <145496630+jhasegaw-student@users.noreply.github.com>
2024-04-16 10:49:34 +0900

    I did homework2.py
    I did homework2.py

---
+++ 2024_spring/lec02/my_first_script.py
@@ -0,0 +1,16 @@
+"""
+This is a docstring for my_first_script.
+A docstring explains what the file does.
+"""
+
+
+# This is a comment
+
+print("hello world")
+
+print(5+6) 
+print(9-4) 
+print(9/3) 
+print(5**3) 
+print(13%5)  # modulo
+print(13//5) # integer division
---
+++ 2024_spring/lec02/my_second_script.py
@@ -0,0 +1,20 @@
+"""
+This is a docstring.
+It explains what the code should do, and how to use it.
+
+my_second_script:
+prints "this is some text"
+"""
+
+# This is a comment
+
+
+print("This is some text")
+
+
+print(5+6) 
+print(9-4) 
+print(9/3) 
+print(5**3)   # 5 to the 3rd power
+print(13%5)   # 13 modulo 5
+print(13//5) # 13 divided by 5 without fractional part (integer)
