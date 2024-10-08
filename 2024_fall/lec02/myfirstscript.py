d099e214dbcf47abeee1675ad5401bc2ab93a9cd
jhasegaw-student <145496630+jhasegaw-student@users.noreply.github.com>
2024-10-07 21:55:04 -0400

    hw2
    hw2

---
+++ 2024_fall/lec02/myfirstscript.py
@@ -0,0 +1,11 @@
+""" This is a script that I am creating as part of lecture 2 in the Speech Understanding course. """
+
+
+print("Hello world, this is python!")
+
+print('5+6 is',5+6) 
+print('9-4 is',9-4) 
+print('13/5 is',13/5) 
+print('to the power of: 5**3 is',5**3) 
+print('modulo (remainder) operator: 13%5 is',13%5) 
+print('integer division: 13//5 is',13//5)
