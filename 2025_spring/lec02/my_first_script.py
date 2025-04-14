b3dfc9ca85094b33785ffed672cf526a050c3170
jhasegaw-student <145496630+jhasegaw-student@users.noreply.github.com>
2025-04-13 20:18:30 -0500

    I finished homework 2
    This is just the changes that finish hw2

---
+++ 2025_spring/lec02/my_first_script.py
@@ -0,0 +1,24 @@
+"""
+
+This is a docstring.
+
+It describes the content of a program.
+This program is just an example script.
+All it does is print the words 'Hello world'.
+
+The end!
+
+"""
+
+# This is a python program
+# This line is a comment
+# The interpreter ignores it
+
+print("Hello world")
+
+print('5+6 is', 5+6) 
+print('9-4 is', 9-4) 
+print('9/3 is', 9/3) 
+print('5**3 is', 5**3) 
+print('13%5 is', 13%5) 
+print('13//5 is', 13//5) 
